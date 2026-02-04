a88f22b205bdb7f97e1b646f4e013e0cf4f12b1a
jafar <hjaafar645@gmail.com>
2026-02-05 00:40:18 +0300

    Electro-Zone

---
+++ js/electro.js
@@ -0,0 +1,153 @@
+/* offCanvas*/
+document.querySelectorAll('.offcanvas a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        const target = link.getAttribute('href');
+        const offcanvasEl = document.querySelector('.offcanvas.show');
+
+        if (offcanvasEl) {
+            const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl);
+            bsOffcanvas.hide();
+
+            offcanvasEl.addEventListener('hidden.bs.offcanvas', () => {
+                document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
+            }, { once: true });
+        }
+    });
+});
+
+/* Swiper */
+
+var swiper = new Swiper(".mySwiper", {
+    speed: 11000,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+
+});
+
+/*Swiper2*/
+
+var swiper = new Swiper(".mySwiper2", {
+
+    speed: 13000,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+/*Swiper 3*/
+
+var swiper = new Swiper(".mySwiper3", {
+
+    speed: 8000,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+/* offerCard */
+const cards = document.querySelectorAll('.offer-card');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+cards.forEach(card => observer.observe(card));
+
+
+/* About */
+const aboutContent = document.querySelector('.about-content');
+const observers = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      aboutContent.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 });
+
+observers.observe(aboutContent);
+
+/* About-content-img */
+const aboutContents = document.querySelector('.about-content');
+
+const observerContent = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      aboutContent.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 });
+
+observerContent.observe(aboutContent);
+
+const images = document.querySelectorAll('.about-section-left img');
+
+const observerImages = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const index = Array.from(images).indexOf(entry.target);
+      setTimeout(() => {
+        entry.target.classList.add('visible');
+      }, index * 300);
+    }
+  });
+}, { threshold: 0.3 });
+
+images.forEach(img => observerImages.observe(img));
